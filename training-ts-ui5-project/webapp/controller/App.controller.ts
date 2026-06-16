09e157f59bd1e3634df44548228de65dc18ae93c
Nam Nguyen <stghoainam4002@gmail.com>
2026-06-16 21:33:08 +0700

    Init TS Project

---
+++ training-ts-ui5-project/webapp/controller/App.controller.ts
@@ -0,0 +1,11 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace trainingtsui5project.controller
+ */
+export default class App extends Controller {
+
+    public onInit(): void {
+
+    }
+}
